7e0ca817220be8b3e139a754a2cf533baab846dc
ferocromay <fernando.ocromay@gmail.com>
2018-09-25 22:04:28 -0500

    Added Home and Settings blank Fragments.

---
+++ app/src/main/java/com/ruzaaniapps/wkquiz/SettingsFrag.kt
@@ -0,0 +1,40 @@
+package com.ruzaaniapps.wkquiz
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SettingsFrag : Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.frag_settings, container, false)
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        private var listener: OnFragmentInteractionListener? = null
+
+        @JvmStatic
+        fun newInstance() = SettingsFrag().apply { }
+    }
+}
